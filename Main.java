2403a1c345dbc27d72483cb4c63a6211beff0ea3
JoachimJaafar <46132781+JoachimJaafar@users.noreply.github.com>
2019-04-14 18:37:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -75,7 +75,6 @@ public class Main extends JPanel implements ActionListener {
         group3.add(j1Button);
         group3.add(j2Button);
  
-        //Register a listener for the radio buttons.
         iaButton.addActionListener(
 		new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -97,7 +96,6 @@ public class Main extends JPanel implements ActionListener {
         j2Button.addActionListener(act_coul);
  
  
-        //Put the radio buttons in a column in a panel.
         JPanel panel = new JPanel(new GridLayout(1, 0));
         panel.add(new JLabel("Mode de jeu : "));
         panel.add(iaButton);
@@ -146,9 +144,7 @@ public class Main extends JPanel implements ActionListener {
         
     }
  
-    /** Listens to the radio buttons. */
     public void actionPerformed(ActionEvent e) {
-        //choix = e.getActionCommand();
     }
     
     ActionListener act_diffi = new ActionListener() {
@@ -163,19 +159,12 @@ public class Main extends JPanel implements ActionListener {
 		    						   }
         						   };
  
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
     private static void createAndShowGUI() {
-        //Create and set up the window.
         JFrame frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
-        //Create and set up the content pane.
         JComponent newContentPane = new Main();
-        newContentPane.setOpaque(true); //content panes must be opaque
+        newContentPane.setOpaque(true);
         frame.setContentPane(newContentPane);
  
         //Display the window.
@@ -184,8 +173,6 @@ public class Main extends JPanel implements ActionListener {
     }
  
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
